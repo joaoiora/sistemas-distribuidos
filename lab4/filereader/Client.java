d03d16a19a2f06b556bc9fd972b7d1db05ec4522
joaoiora <joaoiora.42@gmail.com>
2017-05-05 16:41:44 -0300

    Implemented Server and Client program that reads the content of a given file.

--- lab4/filereader/Client.java
+++ lab4/filereader/Client.java
@@ -1,25 +1,82 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.net.Socket;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Client {
-    
+
     /**
      * 
      */
     public static void main(String[] args) {
-        // TODO Add handling for cmd arguments.
+        String hostname = "localhost";
+        int port = 7896;
+        if (args.length != 2) {
+            System.err.println("usage: Client <hostname> <port>");
+            System.err.println("Invalid number of arguments, will use as default " + hostname + ":" + port + ".");
+        } else {
+            hostname = args[0];
+            port = getPort(args[1]);
+        }
         Client client = new Client();
-        client.connectTo("localhost", 7896);
+        client.connectTo(hostname, port);
     }
 
     /**
      * 
      */
     public void connectTo(final String ip, final int port) {
-        try (Socket socket = new Socket(ip, port)) {
-            
+        try (Socket socket = new Socket(ip, port);
+                PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            System.out.print(input.readLine());
+            output.println(getInput());
+            String line = "";
+            while ((line = input.readLine()) != null) {
+                System.out.println(line);
+            }
         } catch (IOException ex) {
 
         }
+    }
+
+    /**
+     * 
+     */
+    private String getInput() {
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(System.in))) {
+            return input.readLine();
+        } catch (IOException e) {
+            log("[CLIENT] ");
+            log(e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * 
+     */
+    private static void log(final String message) {
+        System.out.println(message);
+    }
+
+    /**
+     * 
+     */
+    private static int getPort(String args) {
+        try {
+            return Integer.parseInt(args);
+        } catch (NumberFormatException ex) {
+            System.err.println("Port Number must be an integer.");
+            System.exit(-1);
+        }
+        return -1;
     }
 
 }
--- lab4/filereader/Server.java
+++ lab4/filereader/Server.java
@@ -1,62 +1,95 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
 
-    /**
-     * 
-     */
-    private int port = 7896;
+	/**
+	 * 
+	 */
+	public static void main(String[] args) {
+		int port = 7896;
+		if (args.length != 1) {
+			System.err.println("usage: Server <portnumber>");
+			System.err.println("Invalid number of arguments, will use as default port " + port + ".");
+		} else {
+			port = getPort(args[0]);
+		}
+		Server server = new Server();
+		server.start(port);
+	}
 
-    /**
-     * 
-     */
-    public static void main(String[] args) {
-        // TODO Add handling for cmd arguments.
-        Server server = new Server();
-        server.start();
-    }
+	/**
+	 * 
+	 */
+	public void start(final int port) {
+		try (ServerSocket serverSocket = new ServerSocket(port)) {
+			log("[SERVER] Server started and listening on port " + port);
+			while (true) { // TODO Change for a word that terminates the connection.
+				try (Socket socket = serverSocket.accept();
+						PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+						BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+					log("[SERVER] Client connected from IP: " + socket.getInetAddress().getLocalHost());
 
-    /**
-     * 
-     */
-    public void start() {
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
-            log("[SERVER] Server started and listening on port " + port);
-            while (true) { // TODO Change for a word that terminates the connection.
-                try (Socket socket = serverSocket.accept();
-                        BufferedWriter input = new BufferedWriter(new PrintWriter(socket.getOutputStream(), true));
-                        BufferedReader output = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-                    log("[SERVER] Client connected from IP: " + socket.getInetAddress().getLocalHost());
-                    write(socket, "[SERVER] Please provide a path: ");
-                    String fromUser = read(socket);
-                    write(socket, proccessInput(fromUser));
-                } catch (IOException socketEx) {
-                    // TODO handle this later
-                }
-            }
-        } catch (IOException serverSocketEx) {
-            // TODO handle this later
-        }
-    }
+					output.println("[SERVER] Please provide a file for reading: ");
 
-    private void write(Socket socket, String message) {
+					String fromUser = input.readLine();
+					log("[SERVER] Client sent `" + fromUser + "`.");
+					// TODO treat user input. What if he sends a directory?
+					String toUser = proccessInput(fromUser);
+					if (toUser == null) {
+						output.println("[SERVER] There was a problem reading the provided file.");
+					} else {
+						output.println(toUser);
+					}
 
-        cleanup();
-    }
+				} catch (IOException socketEx) {
+					log("[SERVER] ");
+				}
+			}
+		} catch (IOException serverSocketEx) {
+			log("[SERVER] ");
+		}
+	}
 
-    private String read(Socket socket) {
+	private static String proccessInput(final String fromUser) {
+		try (BufferedReader fileReader = new BufferedReader(new FileReader(fromUser))) {
+			StringBuilder sb = new StringBuilder();
+			String line = "";
+			while ((line = fileReader.readLine()) != null) {
+				sb.append(line).append(System.lineSeparator());
+			}
+			return sb.toString();
+		} catch (IOException e) {
+			log("[SERVER] ");
+		}
+		return null;
+	}
 
-        cleanup();
-    }
+	/**
+	 * 
+	 */
+	private static void log(final String message) {
+		System.out.println(message);
+	}
 
-    private void cleanup() {
-
-    }
-
-    private static void log(final String message) {
-
-    }
+	/**
+	 * 
+	 */
+	private static int getPort(String args) {
+		try {
+			return Integer.parseInt(args);
+		} catch (NumberFormatException ex) {
+			System.err.println("Port Number must be an integer.");
+			System.exit(-1);
+		}
+		return -1;
+	}
 
 }
